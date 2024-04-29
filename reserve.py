3b7014dbd3607e61edaffb26cc38439b03ce159a
hyunn522 <tjgus52213@gmail.com>
2024-04-29 09:24:06 +0900

    Fix: theater index 변경

--- reserve.py
+++ reserve.py
@@ -317,7 +317,7 @@ def validate_seat_number(choice):
 
     if not ('A' <= row and row <= 'E'):
         return False
-    if not column.isdigit() or int(column) < 1 or 5 < int(column):
+    if not column.isdigit() or int(column) < 0 or 4 < int(column):
         return False
     return True
 
