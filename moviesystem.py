a4482e3af4304e302e14a0b0397f576d28725d84
chrin05 <rinring105@gmail.com>
2024-04-29 10:27:26 +0900

    fix : 불필요한 주석 삭제

--- moviesystem.py
+++ moviesystem.py
@@ -287,9 +287,6 @@ def validate_time_semantics(time_str):
     return True
 
 
-# todo : def check_schedule(date_str, time_str): # 입력한 날짜 뒤에 영화 스케쥴이 있는지 확인
-
-
 def input_date_time():
     while True:
         print("[날짜 및 시간 입력]")
