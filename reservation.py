80d956605675041785a63ba9722c2cee1916f403
hyunn522 <tjgus52213@gmail.com>
2024-06-03 00:24:51 +0900

    fix: 예매 변경 시 쿠폰 관련 error

--- reservation.py
+++ reservation.py
@@ -322,6 +322,7 @@ def print_change_reservation_menu(user_id) :
     # 예매 변경을 위한 예매 아이디 추출
     reservation_id_list = [reservation[0] for reservation in reservation_table]
 
+    choice = ""
     while True:
         print("예매변경할 예매아이디를 입력해주세요")
         choice = input("예매아이디 입력: ")
@@ -353,5 +354,5 @@ def print_change_reservation_menu(user_id) :
 
     # 결제 부분 추가 후 예매한 가격을 가져오는 코드
     # TODO: 인자에 예약아이디 추가해야 함
-    cancel_reservation(choice, ticket_list)  # 변경을 위한 예매 취소 
+    cancel_reservation(choice, choice, ticket_list)  # 변경을 위한 예매 취소 
     reserve.reserve_change(user_id, schedule_id, before_cost, coupon_price) # 취소 후 좌석을 기준으로 예매 변경하기
